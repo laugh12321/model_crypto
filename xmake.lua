7537c379ff2e45ffb91b386dd71000f3a1ab1131
laugh12321 <laugh12321@vip.qq.com>
2023-10-24 10:42:23 +0800

    🔧 Updated xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -5,37 +5,46 @@ add_requires("cryptopp", "pybind11")
 set_languages("c++17")
 
 
-target("core")
+target("crypto_core")
     set_kind("static")
     add_packages("cryptopp")
     add_files("model_crypto/core/*.cpp")
     add_includedirs("model_crypto/core", {interface = true})
 
 
-target("utils")
+target("crypto_utils")
     set_kind("static")
     add_headerfiles("model_crypto/utils/*.h")
     add_includedirs("model_crypto/utils", {interface = true})
 
 
-target("model_crypto")
-    add_deps("core")
+target("core")
+    add_rules("python.library", {soabi = true})
+    add_deps("crypto_core")
     set_kind("shared")
-    set_extension(".pyd")
     add_packages("cryptopp", "pybind11")
-    add_files("model_crypto/pybind/model_crypto.cpp")
+    add_files("model_crypto/pybind/core.cpp")
 
-    after_build(function (target)
-            os.cp(
-                "$(scriptdir)/build/**.pyd", 
-                "$(scriptdir)/python/model_crypto/libs"
-            )
-        end
-    )
+
+target("utils")
+    add_rules("python.library", {soabi = true})
+    add_deps("crypto_utils")
+    set_kind("shared")
+    add_packages("pybind11")
+    add_files("model_crypto/pybind/utils.cpp")
 
 
 target("Crypto")
     set_kind("binary")
     add_packages("cryptopp")
-    add_deps("core", "utils")
+    add_deps("crypto_core", "crypto_utils")
     add_files("model_crypto/main.cpp")
+
+
+after_build(function (target)
+        os.cp(
+            "$(scriptdir)/build/**.pyd", 
+            "$(scriptdir)/python/model_crypto/libs"
+        )
+    end
+)
